5692b19589977b42a54087aef83dcabc4a678175
zuphini <40836255+zuphini@users.noreply.github.com>
2021-08-13 17:22:40 -0700

    Adding Drum-Kit repository

---
+++ index.js
@@ -0,0 +1,79 @@
+
+$(".drum").click(function () {
+
+  var btnInnerHTML = this.innerHTML;
+
+  makeSound(btnInnerHTML);
+  btnAnnimation(btnInnerHTML);
+
+});
+
+$("body").keydown(function (event) {
+
+  makeSound(event.key);
+  btnAnnimation(event.key);
+
+});
+
+function makeSound(key) {
+
+  switch (key) {
+
+    case "a":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+      break;
+
+    case "s":
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+      break;
+
+    case "d":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+      break;
+
+    case "f":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+      break;
+
+    case "j":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+      break;
+
+    case "k":
+        var kick = new Audio('sounds/kick-bass.wav');
+        kick.play();
+      break;
+
+    case "l":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+      break;
+
+    default: console.log(btnInnerHTML);
+
+  }
+
+}
+
+function btnAnnimation(currentKey) {
+
+  var activeBtn = document.querySelector("." + currentKey);
+
+  activeBtn.classList.add("pressed");
+
+  setTimeout(function(){
+    activeBtn.classList.remove("pressed");
+  }, 75);
+
+}
+
+
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
